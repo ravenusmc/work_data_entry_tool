a360c7b0f7a981489582117b351fb2df5e2ac3e5
ravenusmc <mcuddy77@gmail.com>
2023-01-23 15:03:23 -0500

    minor changes with setting up usersign up

--- server/db.py
+++ server/db.py
@@ -9,10 +9,27 @@ from datetime import datetime
 
 class Connection():
 
-	 def __init__(self):
+    def __init__(self):
         self.conn = mysql.connector.connect(user='ted',
                                             password='pass',
                                             host='localhost',
                                             port=3306,
                                             database='work_data_entry_tool')
         self.cursor = self.conn.cursor()
+
+    def encrypt_pass(self, post_data):
+        password = post_data['password'].encode('utf-8')
+        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        return hashed
+
+    # This method will insert a new user into the database.
+    def insert(self, user, hashed):
+        user_created = True
+        self._SQL = """insert into users
+      (firstName, lastName, email, username, password)
+      values
+      (%s, %s, %s, %s, %s)"""
+        self.cursor.execute(self._SQL, (user.first_name,
+                            user.last_name, user.email, user.username, hashed))
+        self.conn.commit()
+        return user_created
