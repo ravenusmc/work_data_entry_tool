94a0607f5e9bd75746dfa877004e342b88752dbb
ravenusmc <mcuddy77@gmail.com>
2023-02-15 16:59:08 -0500

    working on building sample data set

--- server/SQL/action_creator.py
+++ server/SQL/action_creator.py
@@ -15,14 +15,12 @@ class Creator():
         full_action_number = action_type + ending
         return full_action_number
     
-    def create_user_id(self):
-        support = Support()
-        user_id = support.random_number_generator(0,4)
+    def create_user_id(self, obj):
+        user_id = obj.random_number_generator(0,4)
         return user_id
     
-    def create_recruit_action_boolean(self):
-        support = Support()
-        rand_int = support.random_number_generator(0,1)
+    def create_recruit_action_boolean(self, obj):
+        rand_int = obj.random_number_generator(0,1)
         values = [True, False]
         selected_value = values[rand_int]
         return selected_value
@@ -32,8 +30,35 @@ class Creator():
         titles = ['Promotion', 'Chg in Duty Station', 'Accession', 'Conversion']
         title = titles[rand_int]
         return title 
+    
+    def create_date(self, obj, date_type):
+        year = '2023-'
+        rand_month_selector = obj.random_number_generator(0,1)
+        # months = ['01-', '02-']
+        month = '01-'
+        if date_type == 'create_date':
+            random_day = obj.random_number_generator(1,5)
+        elif date_type == 'effective_date':
+            random_day = obj.random_number_generator(25,30)
+        elif date_type == 'received_by_class':
+            random_day = obj.random_number_generator(6,10)
+        elif date_type == 'received_by_staffing':
+            random_day = obj.random_number_generator(11,15)
+        elif date_type == 'received_by_processing':
+            random_day = obj.random_number_generator(16,20)
+        date = year + month + str(random_day)
+        return date
+    
+    def create_NOA(self, title):
+        if title == 'Promotion':
+            return 702
+        elif title == 'Chg in Duty Station':
+            return 792
+        elif title == 'Accession':
+            return 101
+        elif title == 'Conversion':
+            return 500
 
-        
 
 
 # 'action_number': 'TST-TST-2023-0002',
