e8dc0227cb19e3b6b49dd06da7abb4914c02331f
ravenusmc <mcuddy77@gmail.com>
2023-03-17 13:54:08 -0400

    working on drill down for table

--- client/src/components/Graphs/GraphCard.vue
+++ client/src/components/Graphs/GraphCard.vue
@@ -23,15 +23,15 @@ export default {
       chartEvents: {
         select: () => {
           // this.modalTitle = "Drill Down Data for ";
-          console.log(this.data) // This will show you the data
-          // const chart = this.$refs.gChart.chartObject;
-          // const selection = chart.getSelection()[0];
+          // console.log(this.data) // This will show you the data
+          const chart = this.$refs.gChart.chartObject;
+          const selection = chart.getSelection()[0];
           // I need to add one to the row because the first row contains the
           // column headers.
-          // let row = selection.row + 1;
+          let row = selection.row + 1;
           // This pulls out the specific date from the element that the user
           // clicked on
-          // let date = this.data[row][0];
+          let ieNumber = this.data[row][0];
           // const payload = {
           //   date,
           // };
