2e856ed06b027b4cea0f2e1e21565f0504100ce0
ravenusmc <mcuddy77@gmail.com>
2023-04-04 17:03:16 -0400

    working on filter form form table

--- client/src/components/main/Actions.vue
+++ client/src/components/main/Actions.vue
@@ -1,10 +1,53 @@
 <template>
   <div>
-    <hr>
+    <hr />
     <div>
       <h1>Welcome {{ userObject.first_name }}, here are your Actions:</h1>
       <form>
-        
+        <div class="form-group">
+          <label for="actionNumber">Action Number:</label>
+          <input
+            type="text"
+            name="action_number"
+            id="actionNumber"
+            size="20"
+            v-model="action_number"
+            placeholder="Enter Action Number"
+          />
+        </div>
+        <div class="form-group">
+          <label for="recruitAction">Recruit Action:</label>
+          <select id="recruitAction" v-model="selectedValueRecruitAction">
+            <option disabled value="">Please select one</option>
+            <option v-for="item in recruit_action" :key="item" :value="item">
+              {{ item }}
+            </option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="NOA">Nature of Action:</label>
+          <input type="text" name="NOA" id="NOA" size="20" v-model="NOA" />
+        </div>
+        <div class="form-group">
+          <label for="Authority">Authority:</label>
+          <input
+            type="text"
+            name="Authority"
+            id="Authority"
+            size="20"
+            v-model="Authority"
+          />
+        </div>
+        <div class="btn-div">
+          <button
+            @click.prevent="filterTable"
+            type="submit"
+            name="login"
+            class="btn btn-primary button-fix"
+          >
+            Submit
+          </button>
+        </div>
       </form>
       <GraphCard
         :typeOne="typeOne"
@@ -24,21 +67,13 @@ export default {
   components: {
     GraphCard,
   },
-  computed: {
-    ...mapGetters("session", ["userObject", "actionsByUser"]),
-    ...mapGetters("data", ["actionsByUser"]),
-  },
-  methods: {
-    ...mapActions("data", ["getSpecificActionsByUser"]),
-    submitAction() {
-      const payload = {
-        action_number: this.actionNumber,
-      };
-      this.submitMissingActionToDatabase({ payload });
-    },
-  },
   data() {
     return {
+      action_number: "",
+      recruit_action: [true, false],
+      selectedValueRecruitAction: null,
+      NOA: "",
+      Authority: "",
       division_name: "Bottoms",
       typeOne: "Table",
       chartOptionsOne: {
@@ -59,6 +94,20 @@ export default {
       },
     };
   },
+  computed: {
+    ...mapGetters("session", ["userObject", "actionsByUser"]),
+    ...mapGetters("data", ["actionsByUser"]),
+  },
+  methods: {
+    ...mapActions("data", ["getSpecificActionsByUser"]),
+    filterTable() {
+      console.log("Hi");
+      // const payload = {
+      //   action_number: this.actionNumber,
+      // };
+      // this.submitMissingActionToDatabase({ payload });
+    },
+  },
   mounted() {
     const payload = {
       action_number: 7,
@@ -69,4 +118,11 @@ export default {
 </script>
 
 <style scoped>
+
+form {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
 </style>
