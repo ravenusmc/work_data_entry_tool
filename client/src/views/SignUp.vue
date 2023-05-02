6fe6a117ba22caedee53772e2ef02a998812bc30
ravenusmc <mcuddy77@gmail.com>
2023-05-02 16:41:06 -0400

    minor change

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,89 +1,87 @@
 <template>
   <div>
     <div class="signup-heading">
-      <h1>Sign Up</h1>
+      <h1>SIGN UP</h1>
     </div>
 
-    <section class="container">
-      <div class="form-div">
-        <form @submit="signup">
-          <div class="form-group form-inline">
-            <label for="firstname">First Name:</label>
-            <input
-              type="text"
-              name="firstname"
-              v-model="firstName"
-              class="form-control"
-              id="firstname"
-              placeholder="Enter First Name"
-            />
-          </div>
-          <div class="form-group form-inline">
-            <label for="lastName">Last Name:</label>
-            <input
-              type="text"
-              name="lastName"
-              class="form-control"
-              id="lastName"
-              v-model="lastName"
-              placeholder="Enter Last Name"
-            />
-          </div>
-          <div class="form-group form-inline">
-            <label for="email">Email:</label>
-            <input
-              type="text"
-              name="email"
-              class="form-control"
-              id="email"
-               v-model="email"
-              placeholder="Enter Email"
-            />
-          </div>
-          <div class="form-group form-inline">
-            <label for="ienumber">IE Number:</label>
-            <input
-              type="text"
-              name="ienumber"
-              class="form-control"
-              id="ieNumber"
-              v-model="ieNumber"
-              placeholder="Enter IE Number"
-            />
-          </div>
-          <div class="form-group form-inline">
-            <label for="exampleInputPassword1">Password: </label>
-            <input
-              type="password"
-              name="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              v-model="password"
-              placeholder="Password"
-            />
-          </div>
-          <div class="form-group form-inline">
-            <label for="exampleInputPassword2">Confirm Password:</label>
-            <input
-              type="password"
-              name="password2"
-              class="form-control"
-              id="exampleInputPassword2"
-              v-model="confirmPassword"
-              placeholder="Confirm Password"
-            />
-          </div>
+    <div class="container">
+      <form @submit="signup">
+        <div class="form-group form-inline">
+          <label for="firstname">First Name:</label>
+          <input
+            type="text"
+            name="firstname"
+            v-model="firstName"
+            class="form-control"
+            id="firstname"
+            placeholder="First Name"
+          />
+        </div>
+        <div class="form-group form-inline">
+          <label for="lastName">Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            class="form-control"
+            id="lastName"
+            v-model="lastName"
+            placeholder="Last Name"
+          />
+        </div>
+        <div class="form-group form-inline">
+          <label for="email">Email:</label>
+          <input
+            type="text"
+            name="email"
+            class="form-control"
+            id="email"
+            v-model="email"
+            placeholder="Email"
+          />
+        </div>
+        <div class="form-group form-inline">
+          <label for="ienumber">IE Number:</label>
+          <input
+            type="text"
+            name="ienumber"
+            class="form-control"
+            id="ieNumber"
+            v-model="ieNumber"
+            placeholder="IE Number"
+          />
+        </div>
+        <div class="form-group form-inline">
+          <label for="exampleInputPassword1">Password: </label>
+          <input
+            type="password"
+            name="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            v-model="password"
+            placeholder="Password"
+          />
+        </div>
+        <div class="form-group form-inline">
+          <label for="exampleInputPassword2">Confirm Password:</label>
+          <input
+            type="password"
+            name="password2"
+            class="form-control"
+            id="exampleInputPassword2"
+            v-model="confirmPassword"
+            placeholder="Confirm Password"
+          />
+        </div>
 
-          <button
-            type="submit"
-            name="login"
-            class="btn btn-primary form-submit-btn"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    </section>
+        <button
+          type="submit"
+          name="login"
+          class="btn btn-primary form-submit-btn"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -141,10 +139,14 @@ export default {
   margin-top: 30px;
 }
 
-.form-div {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.container {
+  width: 500px;
+  clear: both;
+}
+
+.container input {
+  width: 100%;
+  clear: both;
 }
 
 form {
